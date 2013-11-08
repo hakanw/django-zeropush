26c9e7c2c07fe5df629ee15ef45397a931593c25
Håkan Waara <hwaara@gmail.com>
2013-11-08 12:25:17 +0100

    add admin interface for push devices and delayed push notifications

---
+++ zeropush/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from models import DelayedPushNotification, PushDevice
+
+class DelayedPushNotificationAdmin(admin.ModelAdmin):
+    list_display = ["created", "to_user", "alert", "num_tries", "sent"]
+    readonly_fields = ["to_user", "alert", "sound", "sent", "num_tries", "error", "created", "expired", "info"]
+
+class PushDeviceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PushDevice, PushDeviceAdmin)
+admin.site.register(DelayedPushNotification, DelayedPushNotificationAdmin)
