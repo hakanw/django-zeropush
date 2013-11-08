67f1473cf27710af235e33300d154fa5b4c86dea
Håkan Waara <hwaara@gmail.com>
2013-11-08 11:11:41 +0100

    rev project

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name='django-zeropush',
-    version='0.2.3',
+    version='0.3.0',
     author=u'Håkan Waara',
     author_email='hwaara@gmail.com',
     packages=find_packages(),
